03df40ad4432031b1117433f18fb26ef8880a4a8
l-panxianwen01 <L-panxianwen01@qianxin.com>
2020-04-30 21:36:57 +0800

    增加首页中关注页面的卡片

--- lib/home/follow.dart
+++ lib/home/follow.dart
@@ -47,7 +47,73 @@ class _FollowState extends State<Follow> {
       );
     }
 
-    
+    return new Container(
+      color: GlobalConfig.cardBackgroundColor,
+      margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+      child: new FlatButton(
+        onPressed: (){
+          Navigator.of(context).push(new MaterialPageRoute(
+            builder: (context) {
+              return null;
+            } 
+          ));
+        },
+        child: new Column(
+          children: <Widget> [
+            new Container(
+              child: new Row(
+                children: <Widget> [
+                  new Container(
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage(article.headUrl),
+                      radius: 11.0,
+                    ),
+                  ),
+                  new Text(
+                    "  " + article.user + "  " + article.action + "  " + article.time,
+                    style: new TextStyle(color: GlobalConfig.fontColor),
+                  )
+                ]
+              ),
+              padding: const EdgeInsets.only(top: 10.0),
+            ),
+            new Container(
+              child: new Text(
+                article.title,
+                style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, height: 1.3, color: GlobalConfig.dark==true?Colors.white70:Colors.black)
+              ),
+              margin: EdgeInsets.only(top: 6.0, bottom: 2.0),
+              alignment: Alignment.topLeft
+            ),
+            new Container(
+              child: markWidget,
+              margin: EdgeInsets.only(top: 6.0),
+              alignment: Alignment.topLeft
+            ),
+            new Container(
+              child: new Row(
+                children: <Widget> [
+                  new Expanded(
+                    child: new Text(
+                      article.agreeNum.toString() + "赞同 · " + article.commentNum.toString() + "评论",
+                      style: new TextStyle(color: GlobalConfig.fontColor),
+                    )
+                  ),
+                  new PopupMenuButton(
+                    itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+                      new PopupMenuItem<String>(value: "1", child: new Text("屏蔽这个问题")),
+                      new PopupMenuItem<String>(value: "2", child: new Text("取消关注")),
+                      new PopupMenuItem<String>(value: "3", child: new Text("举报")),
+                    ] 
+                  )
+                ]
+              ),
+              padding: EdgeInsets.only(),
+            ),
+          ]
+        )
+      )
+    );
 
   }
 
@@ -57,7 +123,12 @@ class _FollowState extends State<Follow> {
       child: new Container(
         margin: const EdgeInsets.only(top: 5.0),
         child: new Column(
-          children: <Widget>[]
+          children: <Widget>[
+            wordsCard(articleList[0]),
+            wordsCard(articleList[1]),
+            wordsCard(articleList[2]),
+            wordsCard(articleList[3]),
+          ]
         )
       )
     );
